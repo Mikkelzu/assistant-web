a1fb7dd2096b8358185a896aecc37d563cc4cb01
Mikku <michael.lindemans@outlook.com>
2021-03-04 14:41:52 +0100

    more basic setup for some modules to plan out the idea

--- assistant/assistant-helpers.ts
+++ assistant/assistant-helpers.ts
@@ -1,6 +1,8 @@
+import AudioInputModule from '../audio-input/audio-input-module'
 export default class AssistantHelpers {
 
     private _hasPermissions: boolean | undefined
+    private audioInputModule: any
 
     constructor() {
 
@@ -11,5 +13,14 @@ export default class AssistantHelpers {
         // check for microphone access permissions.
 
         return this._hasPermissions
+    }
+
+    public listenForCreateTodo(): void {
+        // create a todo with specific parameters from microphone input
+        // figure out how to do mic input and NLP (natural language processing) - first only support English
+
+        this.audioInputModule = new AudioInputModule()
+
+        
     }
 }
---
+++ audio-input/audio-input-module.ts
@@ -0,0 +1,16 @@
+export default class AudioInputModule {
+
+    constructor() {}
+
+    public processAudioInput(): void {
+        // return the output in text
+    }
+
+    public listen(): void {
+
+    }
+
+    public transcribe(): void {
+        // transcribe and return the data from input
+    }
+}
